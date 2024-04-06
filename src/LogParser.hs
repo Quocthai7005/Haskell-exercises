d05e217896a33bd3d393c2446492247d062e08b8
Nguyen Quoc Thai <nguyenquocthai7005@gmail.com>
2024-04-06 12:48:01 +0700

    Exercise: Log Parser, part 4.

--- src/LogParser.hs
+++ src/LogParser.hs
@@ -2,7 +2,7 @@
 module LogParser (parseMessage, insertMessage, lineStringToLogMessage, buildMessageTree, MessageTree(..)) where
 
 data MessageType = Info | Warning | Error Int deriving (Show, Eq)
-data LogMessage = LogMessage MessageType Int String | Unknown String deriving (Show, Eq)
+data LogMessage = LogMessage MessageType Int String | Unknown String deriving (Show)
 
 unknownLog :: LogMessage
 unknownLog = Unknown "not a log message"
@@ -28,10 +28,22 @@ instance Show MessageTree where
     show Leaf = "Leaf "
     show (Node left logMessage right) = "Node " ++ show logMessage ++ " (" ++ show left ++ ") (" ++ show right ++ ")"
 
+instance Ord LogMessage where
+    (Unknown m1) `compare` (Unknown m2) = m1 `compare` m2
+    (Unknown m1) `compare` (LogMessage _ _ m2) = m1 `compare` m2
+    (LogMessage _ _ m1) `compare` (Unknown m2) = m1 `compare` m2
+    (LogMessage _ s1 _) `compare` (LogMessage _ s2 _) = s1 `compare` s2
+
+instance Eq LogMessage where
+    (Unknown m1) == (Unknown m2) = m1 == m2
+    (Unknown m1) == (LogMessage _ _ m2) = m1 == m2
+    (LogMessage _ _ m1) == (Unknown m2) = m1 == m2
+    (LogMessage _ s1 _) == (LogMessage _ s2 _) = s1 == s2
+
 insertMessage :: MessageTree -> LogMessage -> MessageTree
 insertMessage mt m = case mt of
                 Leaf -> Node Leaf m Leaf
-                Node t1 _ _ -> insertMessage t1 m
+                Node t1 mx t2 -> if m < mx then Node (insertMessage t1 m) mx t2 else Node t1 mx (insertMessage t2 m)
 
 lineStringToLogMessage :: String -> LogMessage
 lineStringToLogMessage = parseMessage . words
